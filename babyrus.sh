68d10180d53f1749ed44266addf8c23611346eb5
gunitinug <gunitinug@gmail.com>
2026-02-08 16:30:42 +1100

    ver0.99ww

--- babyrus.sh
+++ babyrus.sh
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-BABYRUS_VERSION='v.0.99w'
+BABYRUS_VERSION='v.0.99ww'
 BABYRUS_AUTHOR='Logan Lee'
 
 BABYRUS_PATH="$(pwd)"
