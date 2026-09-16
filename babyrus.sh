ffc6a9caf9a1ede557f8b9f1d0b527c9d2f457ea
gunitinug <gunitinug@gmail.com>
2026-09-16 11:53:48 +1000

    do stuff shortlisted:open url&ebook:sub global

--- babyrus.sh
+++ babyrus.sh
@@ -28474,7 +28474,7 @@ Tag you have chosen will be added to the selected notes." 10 60
                     url=${url_by_id[$selected_url]-}
                     [[ -n $url ]] || continue
 
-                    _spawn google-chrome --new-window -- "$url"
+                    _spawn "$URL_BROWSER" --new-window -- "$url"
                 done
             done
         }
@@ -28705,7 +28705,8 @@ Tag you have chosen will be added to the selected notes." 10 60
 
             case $action in
                 open)
-                    _spawn zathura -- "$ebook"
+                    #_spawn zathura -- "$ebook"
+                    _spawn "$(get_open_command "$ebook")" -- "$ebook"
                     ;;
 
                 bookmarks)
@@ -28780,7 +28781,18 @@ Tag you have chosen will be added to the selected notes." 10 60
                     page=${page_by_id[$bookmark_selection]-}
                     [[ -n $page ]] || return 1
 
-                    _spawn zathura --page="$page" -- "$ebook"
+                    local cmd__ vcmd__
+                    cmd__="$(get_open_command "$ebook")"
+                    vcmd__="${VIEWER_COMMANDS["$cmd__"]}"
+
+                    [[ -n "$vcmd__" ]] || return 1
+
+                    read -r -a viewer_cmd__ <<< "$vcmd__"
+                    _spawn "${viewer_cmd__[@]}" "$page" -- "$ebook"
+
+                    # old.
+                    #_spawn "$vcmd__" "$page" -- "$ebook"                    
+                    #_spawn zathura --page="$page" -- "$ebook"
                     ;;
 
                 *)
